34e62268ef71edb6182c5c0206c30f5a3417a4c6
ttd11204 <datkk112004@gmail.com>
2025-06-03 14:07:43 +0700

    delete comment code

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,21 +1,13 @@
 'use client'
 
 import HeaderLeftSection from '@/components/layout/header/header-left/HeaderLeftSection'
-import HeaderNavigation from '@/components/layout/header/header-left/HeaderNavigation'
 import HeaderRightSection from '@/components/layout/header/header-right/HeaderRightSection'
-import { Button } from '@/components/shadcn/button'
-import { Separator } from '@/components/shadcn/separator'
-import StemifyLogo from '@/components/shared/StemifyLogo'
-import SToolTip from '@/components/shared/SToolTip'
-import { ArrowRightToLine, Bell, Gift, UserPlus } from 'lucide-react'
 
 export default function Header() {
   return (
     <div className='flex flex-col items-center justify-between gap-2 sm:flex-row sm:gap-0'>
-      {/* Left side action */}
       <HeaderLeftSection />
 
-      {/* Right side actions */}
       <HeaderRightSection />
     </div>
   )
