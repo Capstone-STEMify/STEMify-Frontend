ae58087e3295eeed8bfd5e8456a493d16c38a999
ttd11204 <datkk112004@gmail.com>
2025-06-05 23:54:45 +0700

    feat(header): update responsive mobile menu and header layout

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,35 +1,21 @@
-// Header.tsx
-'use client'
-
 import HeaderLeftSection from '@/components/layout/header/header-left/HeaderLeftSection'
 import HeaderRightSection from '@/components/layout/header/header-right/HeaderRightSection'
 import MobileMenu from '@/components/layout/header/MobileMenu'
-import { useEffect, useState } from 'react'
 
 export default function Header() {
-  const [isMobile, setIsMobile] = useState(false)
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 1024)
-    }
-
-    handleResize()
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
   return (
     <header className='sticky top-0 z-50 w-full bg-white/90 shadow-md backdrop-blur-md'>
       <div className='flex items-center justify-between px-4 py-3 sm:px-6 lg:px-8'>
-        {isMobile ? (
+        {/* Hiển thị trên mobile */}
+        <div className='flex w-full items-center justify-between lg:hidden'>
           <MobileMenu />
-        ) : (
-          <>
-            <HeaderLeftSection />
-            <HeaderRightSection />
-          </>
-        )}
+        </div>
+
+        {/* Hiển thị trên desktop */}
+        <div className='hidden w-full items-center justify-between lg:flex'>
+          <HeaderLeftSection />
+          <HeaderRightSection />
+        </div>
       </div>
     </header>
   )
