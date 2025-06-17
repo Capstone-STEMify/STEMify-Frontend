e34cf60f07c1aeae7f7371f3c4a92eb123ad7e25
ttd11204 <datkk112004@gmail.com>
2025-06-17 10:57:49 +0700

    ignore eslint rule and delete empty page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,8 @@ const eslintConfig = [
     rules: {
       'prettier/prettier': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn'
+      '@typescript-eslint/no-unused-vars': 'warn',
+      "react/no-unescaped-entities": "off"
     }
   }
 ]
