73b02511e15ce4f4cda3f43827e26afce2beb4a3
ttd11204 <datkk112004@gmail.com>
2025-06-21 22:56:49 +0700

    pagination v2

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,35 +1,18 @@
 'use client'
 
-import { PaginationWithLinks } from '@/components/shared/pagination/pagination-with-links'
-import { mockPaginatedData } from 'app/test/mockPaginatedClassrooms'
-import { usePaginationParams } from 'app/test/usePaginationParams'
-
-export default function PaginationTestPage() {
-  const { pageNumber, pageSize, search, orderBy } = usePaginationParams()
-
-  // Giả lập API mock hỗ trợ search
-  const { items, totalCount } = mockPaginatedData(pageNumber, pageSize, search)
+import RichTextEditor from '@/components/shared/rich-text-editor'
+import { useModal } from '@/providers/ModalProvider'
+import { useState } from 'react'
+export default function Test() {
+  const [post, setPost] = useState('')
+  const onChange = (content: string) => {
+    setPost(content)
+    console.log(content)
+  }
 
   return (
-    <div className='space-y-6'>
-      <h1 className='text-2xl font-bold'>Danh sách mục</h1>
-
-      <ul className='list-disc pl-5'>
-        {items.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-
-      <PaginationWithLinks
-        pageNumber={pageNumber}
-        pageSize={pageSize}
-        totalCount={totalCount}
-        pageSearchParam='pageNumber'
-        pageSizeSelectOptions={{
-          pageSizeOptions: [5, 10, 20, 50],
-          pageSizeSearchParam: 'pageSize'
-        }}
-      />
+    <div className='my-8'>
+      <RichTextEditor content={post} onChange={onChange} />
     </div>
   )
 }
