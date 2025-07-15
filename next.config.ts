c3e02d030dff384d4312547dc4d10c00eb090b9b
ttd11204 <datkk112004@gmail.com>
2025-07-15 20:54:01 +0700

    feat: update image remote patterns in next.config.ts; refactor course form data keys in CreateCourse component; enhance lesson display with line clamping in ContentSection; integrate markdown rendering in LessonContent component

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,9 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: '6234779.fs1.hubspotusercontent-na1.net' },
       { protocol: 'https', hostname: 'github.com' },
-      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' }
+      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
+      { protocol: 'https', hostname: 'classroom.strawbees.com' },
+      { protocol: 'http', hostname: 'res.cloudinary.com' }
     ]
   }
 }
