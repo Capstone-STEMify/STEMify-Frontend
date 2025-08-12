131ee460c714fdcdadbb102e8ecfd93df7a62fd5
LeThanhNhan91 <lethanhnhan09012004@gmail.com>
2025-08-12 15:14:04 +0700

    feat: multi-language for notifications

--- src/features/notification/components/NotificationHeader.tsx
+++ src/features/notification/components/NotificationHeader.tsx
@@ -2,18 +2,20 @@
 import STabs from '@/components/shared/STabs'
 import NotificationAll from '@/features/notification/components/notification-header/NotificationAll'
 import NotificationUnread from '@/features/notification/components/notification-header/NotificationUnread'
+import { useTranslations } from 'next-intl'
 
 export default function NotificationHeader() {
+  const t = useTranslations('Header')
   return (
     <div>
-      <h2 className='mb-1 text-base font-medium text-gray-900 dark:text-gray-100'>Notifications</h2>
+      <h2 className='mb-1 text-base font-medium text-gray-900 dark:text-gray-100'>{t('notifications.title')}</h2>
       <STabs
         customStyle={{
           trigger: 'text-xs'
         }}
         items={[
-          { label: 'All', value: 'all', content: <NotificationAll /> },
-          { label: 'Unread', value: 'unread', content: <NotificationUnread /> }
+          { label: `${t('notifications.all')}`, value: 'all', content: <NotificationAll /> },
+          { label: `${t('notifications.unread')}`, value: 'unread', content: <NotificationUnread /> }
         ]}
         defaultValue='all'
       />
