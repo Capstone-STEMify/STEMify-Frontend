174f65b672003d32386d5e68b4571e76d474d30a
meewaldor <tieumylam91@gmail.com>
2025-08-16 02:38:20 +0700

    feat: enhance CourseDetailPage with course status management and update layout; add buttons for status approval and rejection

--- src/features/resource/course/components/CourseDetailForAdmin.tsx
+++ src/features/resource/course/components/CourseDetailForAdmin.tsx
@@ -1,11 +1,17 @@
+'use client'
 import React from 'react'
 import LessonTable from '../../lesson/components/table/LessonTable'
 import { Button } from '@/components/shadcn/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/shadcn/card'
 import Image from 'next/image'
 import { Badge } from '@/components/shadcn/badge'
-import { Course } from '../types/course.type'
+import { Course, CourseStatus } from '../types/course.type'
 import { SCard } from '@/components/shared/card/SCard'
+import { useUpdateCourseMutation } from '../api/courseApi'
+import { useTranslations } from 'next-intl'
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
+import { Edit } from 'lucide-react'
 const course = {
   id: 1,
   title: 'City Building',
@@ -16,7 +22,7 @@ const course = {
   studentTasks:
     '1. Research how cities are structured and identify common urban elements (e.g., housing, transportation, green spaces).\n2. Design a city layout on paper or using a digital tool, planning zones for residential, commercial, and public services.\n3. Build a physical city model using straws or recycled materials, focusing on stability and space usage.\n4. Present the city model to the class and explain design decisions based on accessibility, sustainability, and community needs.\n5. Reflect on challenges faced during the building process and propose improvements for future development.',
   duration: 310,
-  status: 'PUBLISHED',
+  status: 'PENDING',
   level: 'BEGINNER',
   createdByUserId: 'b7e2c7e2-8c1a-4e2e-9b2a-2e7c8e2a1b3c',
   ageRangeId: 1,
@@ -31,9 +37,30 @@ const course = {
 }
 
 export default function CourseDetailPage() {
+  const t = useTranslations('CourseDetails')
   const createdAt = course.createdDate ? new Date(course.createdDate).toLocaleString() : 'N/A'
   const updatedAt = course.lastModifiedDate ? new Date(course.lastModifiedDate).toLocaleString() : 'N/A'
   const createdBy = course.createdByUserName?.trim() || 'STEMify Staff'
+  const router = useRouter()
+
+  const [updateCourseStatus] = useUpdateCourseMutation()
+  const handleUpdateCourseStatus = async (status: CourseStatus) => {
+    try {
+      await updateCourseStatus({
+        id: course.id,
+        body: {
+          status
+        }
+      }).unwrap()
+      toast.success(`Course status updated to ${status}`)
+    } catch (error) {
+      toast.error('Failed to review course')
+      console.error('Failed to update course status:', error)
+    }
+  }
+  const handleUpdate = () => {
+    router.push(`/admin/course/update/${course.id}`)
+  }
 
   return (
     <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
@@ -45,9 +72,27 @@ export default function CourseDetailPage() {
           content={
             <div className='text-muted-foreground mt-2 flex flex-wrap gap-3 text-sm'>
               <span>Code: {course.code}</span>
-              <span>Status: {course.status}</span>
-              <span>Level: {course.level}</span>
-              <span>Age Range: {course.ageRangeLabel}</span>
+              <span>
+                Status:{' '}
+                <span>
+                  {' '}
+                  <Badge>{course.status}</Badge>
+                </span>
+              </span>
+              <span>
+                Level:{' '}
+                <span>
+                  {' '}
+                  <Badge>{course.level}</Badge>
+                </span>
+              </span>
+              <span>
+                Age Range:{' '}
+                <span>
+                  {' '}
+                  <Badge>{course.ageRangeLabel}</Badge>
+                </span>
+              </span>
             </div>
           }
         />
@@ -68,7 +113,7 @@ export default function CourseDetailPage() {
           content={
             <div className='mt-2 flex gap-2'>
               {course.topicNames.map((topic) => (
-                <Badge key={topic} variant='secondary' className='text-sm'>
+                <Badge key={topic} variant='secondary' className='bg-red-100 text-red-800'>
                   {topic}
                 </Badge>
               ))}
@@ -81,7 +126,7 @@ export default function CourseDetailPage() {
           content={
             <div className='mt-2 flex gap-2'>
               {course.skillNames.map((skill) => (
-                <Badge key={skill} variant='outline' className='text-sm'>
+                <Badge key={skill} variant='outline' className='bg-emerald-100 text-emerald-700'>
                   {skill}
                 </Badge>
               ))}
@@ -94,7 +139,7 @@ export default function CourseDetailPage() {
           content={
             <div className='mt-2 flex gap-2'>
               {course.standardNames.map((standard) => (
-                <Badge key={standard} variant='outline' className='text-sm'>
+                <Badge key={standard} variant='outline' className='text-orange-custom-500 bg-yellow-custom-50'>
                   {standard}
                 </Badge>
               ))}
@@ -131,6 +176,27 @@ export default function CourseDetailPage() {
             </div>
           }
         />
+        {course.status == CourseStatus.PENDING && (
+          <div className='mt-4 space-x-4'>
+            <Button
+              size='default'
+              className='shadow-6 bg-red-600 font-semibold text-white'
+              onClick={() => handleUpdateCourseStatus(CourseStatus.REJECTED)}
+            >
+              {t('enrolled.action.reject')}
+            </Button>
+            <Button
+              size='default'
+              className='shadow-6 bg-green-600 font-semibold text-white'
+              onClick={() => handleUpdateCourseStatus(CourseStatus.PUBLISHED)}
+            >
+              {t('enrolled.action.approve')}
+            </Button>
+            <Button onClick={handleUpdate} className='bg-sky-custom-600 shadow-6 font-semibold text-white'>
+              {t('notEnrolled.button.update')}
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   )
