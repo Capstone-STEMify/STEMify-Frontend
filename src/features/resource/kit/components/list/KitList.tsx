b2e3ba0aa5eba90a4502a5d08a8f8d0d973dd53e
meewaldor <tieumylam91@gmail.com>
2025-09-07 20:25:57 +0700

    feat: integrate pagination in KitList component; refactor queryParams and enhance layout

--- src/features/resource/kit/components/list/KitList.tsx
+++ src/features/resource/kit/components/list/KitList.tsx
@@ -2,9 +2,11 @@
 import CardLayout from '@/components/shared/card/CardLayout'
 import SEmpty from '@/components/shared/empty/SEmpty'
 import { SDropDown } from '@/components/shared/SDropDown'
+import { SPagination } from '@/components/shared/SPagination'
 import { useDeleteKitMutation, useSearchKitQuery } from '@/features/resource/kit/api/kitApi'
+import { setPageIndex } from '@/features/resource/kit/slice/kitSlice'
 import { KitSliceParams } from '@/features/resource/kit/types/kit.type'
-import { SliceQueryParams } from '@/libs/redux/createQuerySlice'
+import { useAppDispatch, useAppSelector } from '@/hooks/redux-hooks'
 import { useModal } from '@/providers/ModalProvider'
 import { EllipsisVertical } from 'lucide-react'
 import { useTranslations } from 'next-intl'
@@ -18,14 +20,15 @@ export default function KitList() {
   const tt = useTranslations('toast')
 
   const { openModal } = useModal()
+  const dispatch = useAppDispatch()
 
-  const queryParams: KitSliceParams = {
-    pageNumber: 1,
-    pageSize: 8,
-    search: ''
-  }
+  const queryParams : KitSliceParams = useAppSelector((state) => state.kit)
   const { data: kitData, isLoading } = useSearchKitQuery(queryParams)
   const [deleteKit] = useDeleteKitMutation()
+
+  const handlePageChange = (newPage: number) => {
+    dispatch(setPageIndex(newPage))
+  }
 
   const handleDelete = async (e: React.MouseEvent, kitId: number) => {
     e.stopPropagation()
@@ -51,37 +54,50 @@ export default function KitList() {
   }
 
   return (
-    <div className='grid h-fit grid-cols-1 justify-items-center gap-y-10 py-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-      {kitData.data.items.map((kit) => (
-        <div key={kit.id} className='relative flex min-w-0 gap-1'>
-          <Link href={`/resource/kit/${kit.id}`} className='flex w-fit flex-col justify-between'>
-            <CardLayout imageSrc={kit.kitImages?.[0].url || '/images/fallback.png'} size='sm'>
-              <div>
-                <h3 className='line-clamp-1 text-sm font-semibold text-gray-900'>{kit.name}</h3>
-                <p className='line-clamp-4 text-xs text-gray-600'>{kit.description}</p>
-              </div>
-            </CardLayout>
-          </Link>
+    <div className='px-5 select-none'>
+      <div className='grid h-fit grid-cols-1 justify-items-center gap-y-10 py-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+        {kitData.data.items.map((kit) => (
+          <div key={kit.id} className='relative flex min-w-0 gap-1'>
+            <Link href={`/resource/kit/${kit.id}`} className='flex w-fit flex-col justify-between'>
+              <CardLayout imageSrc={kit.kitImages?.[0].url || '/images/fallback.png'} size='sm'>
+                <div>
+                  <h3 className='line-clamp-1 text-sm font-semibold text-gray-900'>{kit.name}</h3>
+                  <p className='line-clamp-4 text-xs text-gray-600'>{kit.description}</p>
+                </div>
+              </CardLayout>
+            </Link>
 
-          <div key={kit.id} className='absolute top-2 right-2 flex flex-col items-center justify-center gap-1'>
-            <SDropDown
-              trigger={<EllipsisVertical className='mt-2 h-5 w-5 text-white hover:scale-[1.1] hover:text-yellow-400' />}
-              items={[
-                <p key={`update-${kit.id}`} className='text-sm'>
-                  {tc('button.update')}
-                </p>,
-                <button
-                  key={`delete-${kit.id}`}
-                  className='text-sm text-red-500'
-                  onClick={(e) => handleDelete(e, kit.id)}
-                >
-                  {tc('button.delete')}
-                </button>
-              ].filter(Boolean)}
-            />
+            <div key={kit.id} className='absolute top-2 right-2 flex flex-col items-center justify-center gap-1'>
+              <SDropDown
+                trigger={
+                  <EllipsisVertical className='mt-2 h-5 w-5 text-white hover:scale-[1.1] hover:text-yellow-400' />
+                }
+                items={[
+                  <p key={`update-${kit.id}`} className='text-sm'>
+                    {tc('button.update')}
+                  </p>,
+                  <button
+                    key={`delete-${kit.id}`}
+                    className='text-sm text-red-500'
+                    onClick={(e) => handleDelete(e, kit.id)}
+                  >
+                    {tc('button.delete')}
+                  </button>
+                ].filter(Boolean)}
+              />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+
+      {kitData.data.totalPages > 1 && (
+        <SPagination
+          pageNumber={kitData.data.pageNumber}
+          totalPages={kitData.data.totalPages}
+          onPageChanged={handlePageChange}
+          className='pb-10'
+        />
+      )}
     </div>
   )
 }
