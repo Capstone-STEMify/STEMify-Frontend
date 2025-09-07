2f99f60704a590129b605e0c5b5fa6d9d97761ff
ttd11204 <datkk112004@gmail.com>
2025-09-07 17:44:57 +0700

    feat: refactor AdminCurriculumList to use CardHorizontal component for improved layout and functionality

--- src/features/resource/curriculum/components/list/AdminCurriculumList.tsx
+++ src/features/resource/curriculum/components/list/AdminCurriculumList.tsx
@@ -1,20 +1,18 @@
 'use client'
-import { Badge } from '@/components/shadcn/badge'
-import CardLayout from '@/components/shared/card/CardLayout'
 import { SPagination } from '@/components/shared/SPagination'
 import { useModal } from '@/providers/ModalProvider'
 import { useLocale, useTranslations } from 'next-intl'
-import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 import { useAppDispatch } from '@/hooks/redux-hooks'
 import LoadingComponent from '@/components/shared/loading/LoadingComponent'
 import { toast } from 'sonner'
 import SEmpty from '@/components/shared/empty/SEmpty'
-import { getStatusBadgeClass } from '@/utils/badgeColor'
-import { capitalizeFirst } from '@/utils/index'
 import { setPageIndex, setPageSize } from '../../slice/curriculumSlice'
 import { useDeleteCurriculumMutation, useSearchCurriculumQuery } from '../../api/curriculumApi'
+import { SCard } from '@/components/shared/card/SCard'
+import Image from 'next/image'
+import CardHorizontal from '@/components/shared/card/CardHorizontal'
 
 export default function AdminCurriculumList() {
   const t = useTranslations('curriculum')
@@ -44,11 +42,6 @@ export default function AdminCurriculumList() {
       </div>
     )
   }
-  const handleNavigate = (curriculumId?: number) => {
-    if (curriculumId) {
-      router.push(`/resource/curriculum/update/${curriculumId}`)
-    } else router.push('/resource/curriculum/create')
-  }
 
   const handleDelete = async (e: React.MouseEvent, curriculumId: number) => {
     e.stopPropagation()
@@ -72,27 +65,16 @@ export default function AdminCurriculumList() {
 
   return (
     <div>
-      <div className='grid h-fit grid-cols-1 justify-items-stretch gap-y-10 py-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        {rows.map((curriculum: any) => (
-          <Link key={curriculum.id} href={`/${locale}/admin/curriculum/${curriculum.id}`}>
-            <CardLayout
-              imageSrc={curriculum.imageUrl}
-              size='sm'
-              badge={
-                <Badge className={`${getStatusBadgeClass(curriculum.status)}`}>
-                  {capitalizeFirst(curriculum.status)}
-                </Badge>
-              }
-            >
-              <p className='text-amber-custom-400 line-clamp-1 text-xs font-semibold'>{curriculum.code}</p>
-              <h3 className='line-clamp-1 text-base font-semibold text-gray-700'>{curriculum.title}</h3>
-              <p className='line-clamp-2 text-sm text-gray-500'>{curriculum.description}</p>
-
-              <div className='mt-auto'>
-                <Badge className='bg-rose-300'>{curriculum.courseCount || 0} courses</Badge>
-              </div>
-            </CardLayout>
-          </Link>
+      <div className='grid grid-cols-1 gap-10 lg:grid-cols-2'>
+        {rows.map((curriculum) => (
+          <CardHorizontal
+            key={curriculum.id}
+            imageUrl={curriculum.imageUrl}
+            title={curriculum.title}
+            description={curriculum.description}
+            buttonText='View Details'
+            onButtonClick={() => router.push(`/${locale}/admin/curriculum/${curriculum.id}`)}
+          />
         ))}
       </div>
 
