b7a683967a97fa7cf4b9e4ac5be084690a7b306c
meewaldor <tieumylam91@gmail.com>
2025-09-09 08:55:04 +0700

    feat: add list titles for age range, skill, standard, topic in admin pages and update corresponding components

--- src/app/[locale]/admin/(document)/age-range/page.tsx
+++ src/app/[locale]/admin/(document)/age-range/page.tsx
@@ -1,5 +1,12 @@
-import AgeRangeTable from '@/features/resource/age-range/components/list/AgeRangeTable';
+import AgeRangeTable from '@/features/resource/age-range/components/list/AgeRangeTable'
+import { useTranslations } from 'next-intl'
 
 export default function AdminAgeRange() {
-  return <AgeRangeTable/>
+  const t = useTranslations('Admin.ageRange')
+  return (
+    <div>
+      <h1 className='text-2xl font-semibold text-gray-800'>{t('listTitle')}</h1>
+      <AgeRangeTable />
+    </div>
+  )
 }
