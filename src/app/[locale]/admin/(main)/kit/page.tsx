ab8b96370246790aec4e5f0cc5f50136b238abeb
meewaldor <tieumylam91@gmail.com>
2025-09-09 08:50:10 +0700

    feat: update lesson and kit pages to include localized titles and improve layout

--- src/app/[locale]/admin/(main)/kit/page.tsx
+++ src/app/[locale]/admin/(main)/kit/page.tsx
@@ -1,10 +1,13 @@
 import KitList from '@/features/resource/kit/components/list/KitList'
 import KitAction from '@/features/resource/kit/components/list/KitListAction'
+import { useTranslations } from 'next-intl'
 import React from 'react'
 
 export default function KitListPage() {
+  const t = useTranslations('kits')
   return (
     <div>
+      <h1 className='text-2xl font-semibold text-gray-800'>{t('list.title')}</h1>
       <KitAction />
       <KitList />
     </div>
