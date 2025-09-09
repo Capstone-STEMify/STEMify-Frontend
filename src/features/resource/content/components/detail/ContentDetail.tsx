0f127da2bda4142c3328ee60cf2954826b7efea5
meewaldor <tieumylam91@gmail.com>
2025-09-09 19:14:02 +0700

    feat: add internationalization support for content detail and modal components; include English and Vietnamese translations

--- src/features/resource/content/components/detail/ContentDetail.tsx
+++ src/features/resource/content/components/detail/ContentDetail.tsx
@@ -5,12 +5,14 @@ import { useSearchContentQuery } from '@/features/resource/content/api/contentAp
 import { useModal } from '@/providers/ModalProvider'
 import { normalizeMarkdown } from '@/utils/index'
 import { FilePlus } from 'lucide-react'
+import { useTranslations } from 'next-intl'
 
 type ContentDetailProps = {
   sectionId: number
 }
 
 export default function ContentDetail({ sectionId }: ContentDetailProps) {
+  const t = useTranslations('content')
   const { data: contentData, isLoading } = useSearchContentQuery({ sectionId })
   const { openModal } = useModal()
 
@@ -27,11 +29,11 @@ export default function ContentDetail({ sectionId }: ContentDetailProps) {
   if (!contentData?.data?.items?.length)
     return (
       <div className='flex flex-col items-center justify-center space-y-4 rounded-2xl border bg-gray-50 py-10 text-center'>
-        <h3 className='text-lg font-semibold text-gray-800'>No content found</h3>
-        <p className='text-gray-500'>You haven’t created any content yet.</p>
+        <h3 className='text-lg font-semibold text-gray-800'>{t('detail.noData')}</h3>
+        <p className='text-gray-500'>{t('detail.noDataDetail')}</p>
         <Button onClick={handleCreateContent} className='bg-amber-custom-400 flex items-center gap-2'>
           <FilePlus className='h-4 w-4' />
-          Create Content
+          {t('form.title.create')}
         </Button>
       </div>
     )
