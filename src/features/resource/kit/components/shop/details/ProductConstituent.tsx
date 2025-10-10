a249bfa93da2e76b05319a6ccea117bafd87afcb
meewaldor <tieumylam91@gmail.com>
2025-10-10 09:55:59 +0700

    fix: add conditional rendering for delete button based on user role in WhatsIncluded component

--- src/features/resource/kit/components/shop/details/ProductConstituent.tsx
+++ src/features/resource/kit/components/shop/details/ProductConstituent.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { ChevronDown, Trash2, X } from 'lucide-react'
@@ -7,6 +8,8 @@ import { Badge } from '@/components/shadcn/badge'
 import { toast } from 'sonner'
 import { useTranslations } from 'next-intl'
 import { Component, KitComponent } from '@/features/kit-components/types/kit-component.type'
+import { useAppSelector } from '@/hooks/redux-hooks'
+import { UserRole } from '@/types/userRole'
 
 export type WhatsIncludedProps = {
   components: KitComponent[]
@@ -19,6 +22,10 @@ const WhatsIncluded: React.FC<WhatsIncludedProps> = ({ components, name, addBtn,
   const t = useTranslations('kits')
   const [isExpanded, setIsExpanded] = useState(true)
   const { openModal } = useModal()
+
+  const user = useAppSelector((state) => state.auth?.user)
+  const role = user?.role
+  const isEditable = role === UserRole.ADMIN || role === UserRole.STAFF
 
   const [deleteComponentFromKit] = useDeleteKitComponentsMutation()
 
@@ -68,12 +75,14 @@ const WhatsIncluded: React.FC<WhatsIncludedProps> = ({ components, name, addBtn,
                 <img src={item.imageUrl} alt={item.name} className='h-full w-full object-contain' />
 
                 {/* button delete */}
-                <Badge
-                  onClick={() => handleDelete(item.id as number)}
-                  className='absolute top-1 right-1 cursor-pointer rounded-full bg-red-50 p-1 text-red-500 hover:text-red-600'
-                >
-                  <Trash2 size={14} />
-                </Badge>
+                {isEditable && (
+                  <Badge
+                    onClick={() => handleDelete(item.id as number)}
+                    className='absolute top-1 right-1 cursor-pointer rounded-full bg-red-50 p-1 text-red-500 hover:text-red-600'
+                  >
+                    <Trash2 size={14} />
+                  </Badge>
+                )}
               </div>
               <h4 className='mb-1 text-center leading-tight font-medium text-gray-900'>{item.name}</h4>
               <p className='text-sm text-gray-500'>x{item.quantity}</p>
