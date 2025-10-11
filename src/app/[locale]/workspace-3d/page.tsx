38f2aa6f01fcb89f7da6f473198bcba7f7a15735
ttd11204 <datkk112004@gmail.com>
2025-10-11 18:55:23 +0700

    refactor: remove Workspace3DPage component and its dependency on StrawLabList

--- src/app/[locale]/workspace-3d/page.tsx
+++
@@ -1,9 +0,0 @@
-import StrawLabList from '@/features/straw-lab/components/StrawLabList'
-
-export default function Workspace3DPage() {
-  return (
-    <div>
-      <StrawLabList />
-    </div>
-  )
-}
