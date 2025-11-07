dde50ce2482cd0022096427bcc1ffd73f1f04278
ttd11204 <datkk112004@gmail.com>
2025-11-07 16:16:47 +0700

    feat: refactor assignment components by removing unused files and updating submission form with enhanced functionality and state management

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -1,6 +1,9 @@
-import UpsertAssignment from '@/features/assignment/components/upsert/UpsertAssignment'
+'use client'
+import { AssignmentAttemptDemo } from '@/features/assignment/components/attempt/AssigmentAttempt' // trang làm có nút bấm làm bài
+import { AssignmentSubmissionFormDemo } from '@/features/assignment/components/attempt/AssignmentSubmission' // trang làm bài
+import { AssignmentGradingPageDemo } from '@/features/assignment/components/grading/AssignmentGrading' // trang chấm bài
 import React from 'react'
 
 export default function AAAAAA() {
-  return <UpsertAssignment />
+  return <AssignmentSubmissionFormDemo />
 }
