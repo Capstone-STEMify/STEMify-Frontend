721f16abbdc953fd048859e918403852099fd092
meewaldor <tieumylam91@gmail.com>
2025-11-09 01:23:06 +0700

    feat: update AuthSessionSync to use token from Redux state for user data fetching

--- src/providers/AuthSessionSync.tsx
+++ src/providers/AuthSessionSync.tsx
@@ -2,14 +2,16 @@
 
 import { setToken, setUser } from '@/features/auth/authSlice'
 import { useGetUserByIdQuery } from '@/features/user/api/userApi'
-import { useAppDispatch } from '@/hooks/redux-hooks'
+import { useAppDispatch, useAppSelector } from '@/hooks/redux-hooks'
 import { useSession } from 'next-auth/react'
 import { useEffect } from 'react'
 
 export default function AuthSessionSync() {
   const { data } = useSession()
   const dispatch = useAppDispatch()
-  // const { data: userData } = useGetUserByIdQuery(data?.user.userId!, { skip: !data?.user.userId })
+  const token = useAppSelector((state) => state.auth.token)
+
+  const { data: userData } = useGetUserByIdQuery(data?.user.userId!, { skip: !token })
 
   useEffect(() => {
     if (!data) return
