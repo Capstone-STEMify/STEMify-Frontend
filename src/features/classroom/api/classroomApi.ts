b8496c123577b97e08cb27891e08b92bbd7e274e
ttd11204 <datkk112004@gmail.com>
2025-11-16 23:47:12 +0700

    feat(classroom): enhance API with custom query handling for organization and subscription headers

--- src/features/classroom/api/classroomApi.ts
+++ src/features/classroom/api/classroomApi.ts
@@ -1,11 +1,30 @@
 import { Classroom, ClassroomSchedule, ClassroomSliceParams } from '@/features/classroom/types/classroom.type'
-import { createCrudApi } from '@/libs/redux/baseApi'
+import { createCrudApi, customFetchBaseQueryWithErrorHandling } from '@/libs/redux/baseApi'
+import { RootState } from '@/libs/redux/store'
 import { ApiSuccessResponse } from '@/types/baseModel'
 
 export const classroomApi = createCrudApi<Classroom, ClassroomSliceParams>({
   reducerPath: 'classroomApi',
   baseUrl: '/classrooms',
-  tagTypes: ['Classroom']
+  tagTypes: ['Classroom'],
+  baseQuery: async (args, api, extra) => {
+    const activeOrg = (api.getState() as RootState).selectedOrganization.selectedOrganizationId
+    const activeSub = (api.getState() as RootState).selectedOrganization.selectedSubscriptionOrderId
+
+    let headers: Record<string, string> = {}
+    if (activeOrg) headers['X-Active-Organization'] = String(activeOrg)
+    if (activeSub) headers['X-Active-Subscription'] = String(activeSub)
+
+    // gọi baseQuery gốc
+    return customFetchBaseQueryWithErrorHandling(
+      {
+        ...(typeof args === 'string' ? { url: args } : args),
+        headers: { ...headers, ...(typeof args === 'string' ? {} : args.headers) }
+      },
+      api,
+      extra
+    )
+  }
 }).injectEndpoints({
   endpoints: (builder) => ({
     addClassroomStudents: builder.mutation<void, { classroomId: number; studentEmails: string[] }>({
