8e88144ecdefb226775624227edf5a809738ea13
LeThanhNhan91 <lethanhnhan09012004@gmail.com>
2025-12-16 16:15:17 +0700

    feat: refactor message loading logic and remove unnecessary console log in middleware

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,15 +1,18 @@
 import { getRequestConfig } from 'next-intl/server';
 import { hasLocale } from 'next-intl';
 import { routing } from './routing';
-import { loadMessages } from './loadMessages';
+import { getMessages } from './getMessages';
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  const requested = await requestLocale;
-  const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
-  const messages = await loadMessages(locale);
+  const requested = await requestLocale
+  const locale = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale
+
+  const messages = await getMessages(locale)
 
   return {
     locale,
-    messages,
-  };
-});
+    messages
+  }
+})
